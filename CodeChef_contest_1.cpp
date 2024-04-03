86dd6fa8e0e3eaa2ce47ae39fadb0f920e24d3e0
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-03 20:36:43 +0530

    Add files via upload

---
+++ CodeChef_contest_1.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin >> x >> y; 
+	   int rate = x + (y);
+	   
+	   if (rate>=2000)
+	   {
+	       cout << " YES";
+	       
+	   }
+	   else
+	   {
+	       cout << "NO";
+	   }
+}
