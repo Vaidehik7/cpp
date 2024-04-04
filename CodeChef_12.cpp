9296764a114b5f70d82cf021af542baf854a252f
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-04 07:03:50 +0530

    Add files via upload

---
+++ CodeChef_12.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	//X = threshold ji lahan pahije
+	//Y = current brain speed ji mothi pahijel
+	int X,Y;
+	cin>>X>>Y;
+	if(X<Y){
+	    cout<<"YES"<<endl;
+	    
+	}
+	else if(Y=X){
+	    cout<<"NO"<<endl;
+	}
+	else{
+	    
+	    cout<<"YES"<<endl;
+	}
+	    
+	
+	
+	return 0;
+}
