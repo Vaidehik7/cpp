326bd57e48c9e80facce6db6ad6346874331be1f
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-09 12:59:01 +0530

    Add files via upload

---
+++ CodeChef_16.cpp.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y;
+    int sum;
+    //x=mess cost....y=restarant cost
+    cin >> x >> y;
+    for(int i=1; i<=6; i++)
+    {
+       i=x;
+        
+        sum = x*6;
+    }
+    for(int j=1; j<=1; j++)
+    {
+     j=y;
+    }
+    
+    int sum1 = sum + y;
+    cout << sum1;
+    
+    
+}
