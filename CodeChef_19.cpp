85fbaf05b9135a67317e5becb77f26e20d18984a
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-10 20:26:50 +0530

    Add files via upload

---
+++ CodeChef_19.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    //x=rainy, y=cloudy
+    int x, y;
+   
+   cin >> x >> y;
+
+     int sum=7-x-y;
+    cout << sum;
+}
