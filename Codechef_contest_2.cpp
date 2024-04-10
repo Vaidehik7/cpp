eb16103fed8a7d5b024ecb917a9b60c5b377b591
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-10 20:28:05 +0530

    Add files via upload

---
+++ Codechef_contest_2.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    //x=fans of Leo, y=Fans of Ronaldo
+    int x, y;
+    cin >> x >> y;
+    if(x>y)
+    {
+        cout << "FREEKICK";
+    }
+    else
+    {
+        cout << "PENALTY";
+    }
+}
