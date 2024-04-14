4d3679040b4bc84f24792fdbb86abf088a8aa7d2
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-14 16:59:41 +0530

    Add files via upload

---
+++ CodeChef_27.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    //a=alice, b=bob
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if((a+b)%2==0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
