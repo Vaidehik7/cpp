810c1dec6c913d43acf564714076027a94eae79a
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-15 21:14:45 +0530

    Add files via upload

---
+++ CodeChef_28.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X;
+	cin >> X;
+    if(X> 35)
+    {
+        cout << "yes" ;
+    }
+    else if(X==35)
+    {
+        cout << "no";
+    }
+    else
+    {
+        cout << "no" ;
+    }
+}
