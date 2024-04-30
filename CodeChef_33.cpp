7cd2854ae45b9308dc97fcb8212f196dbce11dd1
Vaidehik7 <144208252+Vaidehik7@users.noreply.github.com>
2024-04-30 10:09:19 +0530

    Add files via upload

--- CodeChef_33.cpp
+++ CodeChef_33.cpp
@@ -1,35 +1,25 @@
 #include <bits/stdc++.h>
-#include <iostream>
 using namespace std;
 
-int main() 
-{
-    int t; 
-    cin>>t;
-    while(t--) 
+int main() {
+	// your code goes here
+    int t;
+    cin >> t;
+    while(t--)
     {
-        int n;
-        cin>>n;
-        if(n%2==0) 
-        {
-            string seq,s1;
-            cin>>seq;
-            for(int i=0; i<seq.length()-1;i+=2)
-            {
-                char c1=seq[i];
-                char c2=seq[i+1];
-                if(c1=='0' && c2=='0')
-                s1=s1+'A';
-                if(c1=='0' && c2=='1')
-                s1=s1+'T';
-                if(c1=='1' && c2=='0')
-                s1=s1+'C';
-                if(c1=='1' && c2=='1')
-                s1=s1+'G';
-            }
-            cout << s1 << endl;
-        }
+        string hidden;
+        cin >> hidden;
+        string guess;
+        cin >> guess;
         
+        for(int i=0; i<5; i++)
+        {
+            if(hidden[i]==guess[i])
+            cout<<'G';
+            else
+            cout<<'B';
+        }
+        cout << endl;
     }
-	return 0;
+    return 0;
 }
